ca03de28a4529503566c7d95d27eda048a64b237
Jack Meier <jackson.meier423@gmail.com>
2020-08-13 09:46:41 -0700

    Updates

---
+++ precache-manifest.f8184971eadeafede8a5fe87dfc81a31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d4d7cefca6da5c61bc495fa3628755",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "0c9d3d033fdb6e0447db",
+    "url": "/react-portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "ecd5b1585f89e67593ca",
+    "url": "/react-portfolio/static/js/2.0127c1e9.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/react-portfolio/static/js/2.0127c1e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c9d3d033fdb6e0447db",
+    "url": "/react-portfolio/static/js/main.d51fdf1d.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
