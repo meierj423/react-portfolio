ea81de4f60839b07bc38b3298b0191c97535ed07
Jack Meier <jackson.meier423@gmail.com>
2020-08-26 15:45:02 -0700

    edited alt text for profile picture

--- src/Components/About.js
+++ src/Components/About.js
@@ -17,7 +17,7 @@ class About extends Component {
             <img
               className="profile-pic"
               src={profilepic}
-              alt="Tim Baker Profile Pic"
+              alt="Jack Meier Profile Pic"
             />
           </div>
           <div className="nine columns main-col">
