b0eff613f45899e8ed890b818a923642f156d60f
Jack Meier <jackson.meier423@gmail.com>
2020-09-10 14:09:53 -0700

    added technologies used to each project

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -20,6 +20,8 @@ class Portfolio extends Component {
                   <div className="portfolio-item-meta">
                     <h5>{projects.title}</h5>
                     <p>{projects.category}</p>
+                    <br></br>
+                    <p>{projects.tech}</p>
                   </div>
                 </div>
                 <div className="link-icon">
